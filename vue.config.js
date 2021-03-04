7ae206da3ce4992f9c75af020b486f50082f542e
lrg <18513036945@163.com>
2021-03-04 10:06:00 +0800

    lrg

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,18 @@
 const defaultSettings = require('./src/settings.js')
 
 const titleHtml = defaultSettings.title || 'vue Element Admin' // page title
+// const version = new Date().getTime()
+// const timeStamp = new Date().getTime()
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/'
     : '/',
   productionSourceMap: true,
+  // outputDir: `dist/${version}`,
+  // outputDir: 'dist/1.0.2',
+  // filenameHashing: false, // 打包的时候不使用hash值.因为我们有时间戳来确定项目的唯一性了.
+
   devServer: {
     open: true, // 配置自动启动浏览器
     proxy: {
@@ -30,5 +36,15 @@ module.exports = {
   },
   configureWebpack: config => {
     config.entry.app = ['babel-polyfill', './src/main.js']
+    // config.output = { // 输出重构 打包编译后的js文件名称,添加时间戳.
+    //   filename: `js[nam].${timeStamp}.js`,
+    //   chunkFilename: `js[nam].${timeStamp}.js`
+    // }
+  },
+  css: {
+    // extract: {
+    //   filename: `css/[name].${timeStamp}.css`,
+    //   chunkFilename: `css/[name].${timeStamp}.css`
+    // }
   }
 }
